11bd91c50fbeded39a1853cf0a5d48e10634e36b
Thamyris <tgthurler@gmail.com>
2022-02-15 12:40:22 -0300

    Resolvido questão 02 do desafio para verificar senha

---
+++ desafio-programacao-capgemini/src/com/desafio/capgemini/questoes/Questao02.java
@@ -0,0 +1,40 @@
+package com.desafio.capgemini.questoes;
+
+import java.util.Scanner;
+
+public class Questao02 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		String senha = leInformacao("Por favor digite sua senha para verificação ", scan);
+		verificaSenha(senha);
+
+	}
+
+	protected static String leInformacao(String msg, Scanner scan) {
+
+		System.out.println(msg);
+		String entrada = scan.nextLine();
+
+		return entrada;
+	}
+
+	protected static void verificaSenha(String senha) {
+
+		String regex = "(?=.*[!@#$%^&*()-+])(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{6,}";
+
+		if (senha.length() < 6) {
+			int x = 6 - senha.length();
+			System.out.println("Adicione pelo menos mais " + x + " caracteres");
+		} else {
+			if (senha.matches(regex) && !senha.contains(" ")) {
+				System.out.println("Sua senha é forte");
+			} else {
+				System.out.println("Sua senha é fraca");
+			}
+
+		}
+	}
+
+}
